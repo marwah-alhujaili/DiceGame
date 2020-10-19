790d92895ff39720d3105053dbf2920b3b30d282
Marwah Ahhujaili <71184951+marwah-alhujaili@users.noreply.github.com>
2020-10-20 02:51:28 +0300

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,4 @@
-
-
-      function rollTheDice(){
+function rollTheDice(){
             var randomNum1 = Math.floor(Math.random() * 6) + 1;
             var imgrandompath1='diceimg/dice'+ randomNum1+".png";
 
@@ -12,22 +10,28 @@
   
             document.querySelectorAll('img')[1].
           setAttribute('src', imgrandompath2);
-          var audio1 =new Audio('applause.mp3');
+
 
            if (randomNum1 > randomNum2) {
              document.querySelector('h1').innerHTML= "Player 1 Wins!";
-                            audio1.play();
+             playAudio();
          }
          else if (randomNum1 < randomNum2) {
            document.querySelector('h1').innerHTML= "Player 2 Wins!";
-                          audio1.play();
+                    playAudio();
                    }
 
          else {
                 document.querySelector('h1').innerHTML = "Draw!";
          }
 }
-        document.querySelector('button').addEventListener('click',rollTheDice);
+function playAudio(){
+
+var audio1 =new Audio('applause.mp3');
+  audio1.play();
+
+}
+        document.querySelector('button').addEventListener('click',rollTheDice);
 
         document.querySelector('button').addEventListener('click',function(){
            var audio =new Audio('Clicke_sound.mp3');
