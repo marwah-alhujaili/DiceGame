42e44e4b2150ed3c5a8940f6c48cea145990f1cb
Marwah Ahhujaili <71184951+marwah-alhujaili@users.noreply.github.com>
2020-10-19 14:49:50 +0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,32 @@
+
+
+      function rollTheDice(){
+            var randomNum1 = Math.floor(Math.random() * 6) + 1;
+            var imgrandompath1='diceimg/dice'+ randomNum1+".png";
+
+            var randomNum2 = Math.floor(Math.random() * 6) + 1;
+            var imgrandompath2='diceimg/dice'+ randomNum2+".png";
+
+            document.querySelectorAll('img')[0].
+            setAttribute('src', imgrandompath1);
+  
+            document.querySelectorAll('img')[1].
+          setAttribute('src', imgrandompath2);
+
+           if (randomNum1 > randomNum2) {
+             document.querySelector('h1').innerHTML= "Player 1 Wins!";             
+         }
+         else if (randomNum1 < randomNum2) {
+           document.querySelector('h1').innerHTML= "Player 2 Wins!";
+                     }
+         else {
+                document.querySelector('h1').innerHTML = "Draw!";
+
+         }
+}
+        document.querySelector('button').addEventListener('click',rollTheDice);
+
+        document.querySelector('button').addEventListener('click',function(){
+           var audio =new Audio('Clicke_sound.mp3');
+               audio.play();
+             });
